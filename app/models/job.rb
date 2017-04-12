67bf106be3c9a240a7397e8f74b18d3ddd8d1557
cinly <cinlyma@gmail.com>
2017-04-12 18:10:34 +0800

    step 7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ApplicationRecord
+  has_many :resumes
+
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
@@ -18,5 +20,6 @@ class Job < ApplicationRecord
   scope :recent, -> { order('created_at DESC') }
   scope :high, -> { order('wage_upper_bound DESC')}
   scope :low, -> { order('wage_lower_bound DESC')}
+
 
 end
