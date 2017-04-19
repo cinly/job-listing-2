ee27ab35a39081a80522bf1bc2861f8356ce7828
cinly <cinlyma@gmail.com>
2017-04-19 12:50:40 +0800

    mogai-migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,14 +14,12 @@ class User < ApplicationRecord
 
   has_many :job_relationships
   has_many :applied_jobs, :through => :job_relationships, :source => :job
-  has_many :job_relationships
-  has_many :applied_jobs, :through => :job_relationships, :source => :job
 
   def has_applied?(job)
-  applied_jobs.include?(job)
-end
+   applied_jobs.include?(job)
+  end
 
-def apply!(job)
-  applied_jobs << job
-end
+  def apply!(job)
+   applied_jobs << job
+  end
 end
